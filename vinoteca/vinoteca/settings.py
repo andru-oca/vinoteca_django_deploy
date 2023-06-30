cf3b832eeab76db6067bf3c0ea6a75f7fe619130
andru-oca <112781394+andru-oca@users.noreply.github.com>
2023-06-30 18:14:53 -0300

    update database config

--- vinoteca/vinoteca/settings.py
+++ vinoteca/vinoteca/settings.py
@@ -84,25 +84,31 @@ WSGI_APPLICATION = 'vinoteca.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'database_name',
-#         'USER': 'root',
-#         'PASSWORD': '',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
-#         'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+USUARIO_PYTHON_ANYWHERE     = "celesteGomez"
+PASSWORD_MYSQL              = "vinoteca1234"
+MYSQL_PYTHON_ANYWHERE_HOST  = USUARIO_PYTHON_ANYWHERE+".mysql.pythonanywhere-services.com"
+MYSQL_DATABASE              = USUARIO_PYTHON_ANYWHERE+"$vinoteca"
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': MYSQL_DATABASE
+        'USER': USUARIO_PYTHON_ANYWHERE,
+        'PASSWORD': PASSWORD_MYSQL,
+        'HOST': MYSQL_PYTHON_ANYWHERE_HOST,
+        'PORT': '3306',
+        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
+    }
+}
 
 
 
