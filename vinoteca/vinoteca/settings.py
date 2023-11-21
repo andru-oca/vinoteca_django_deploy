975540f931b5bc2aa086ab00114f5c2dd513862c
andru-oca <112781394+andru-oca@users.noreply.github.com>
2023-11-21 12:27:46 -0300

    Update settings.py

--- vinoteca/vinoteca/settings.py
+++ vinoteca/vinoteca/settings.py
@@ -148,9 +148,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static"]
+    BASE_DIR / "staticfiles"]
 
-# STATIC_ROOT = BASE_DIR / "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
